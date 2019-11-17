7736c06a309f5e825391fe7c89139b708b6a042c
Maerodrim <43945193+Maerodrim@users.noreply.github.com>
2019-11-17 21:36:50 +0400

    Version 4.3 автоматизация

--- GameNetcreckerData/src/main/java/com/example/GameNetcreckerData/Controller/GameTableController.java
+++ GameNetcreckerData/src/main/java/com/example/GameNetcreckerData/Controller/GameTableController.java
@@ -2,11 +2,8 @@ package com.example.GameNetcreckerData.Controller;
 
 
 import com.example.GameNetcreckerData.Dto.View.View;
-import com.example.GameNetcreckerData.Model.Cube;
+import com.example.GameNetcreckerData.Model.*;
 import com.example.GameNetcreckerData.Model.Enum.TableStatus;
-import com.example.GameNetcreckerData.Model.Events;
-import com.example.GameNetcreckerData.Model.GameTable;
-import com.example.GameNetcreckerData.Model.Users;
 import com.example.GameNetcreckerData.Repo.*;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.extern.log4j.Log4j2;
@@ -15,11 +12,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 @RestController
 @RequestMapping("GameTableController")
 @Log4j2
 public class GameTableController {
+    @Autowired
+    private CardRepo cardRepo;
     @Autowired
     private UsersRepo usersRepo;
     @Autowired
@@ -28,6 +28,8 @@ public class GameTableController {
     private GameTableRepo gameTableRepo;
     @Autowired
     private EventsRepo eventsRepo;
+    @Autowired
+    private NullPackCardRepo nullPackCardRepo;
 
     @JsonView(View.CUBA.class)
     @PostMapping("addGameTable")
@@ -44,6 +46,28 @@ public class GameTableController {
     public Integer addUsers(@RequestParam Integer IdGameTable, @RequestParam String email) {
         gameTableRepo.findByIdGameTable(IdGameTable).addUsers(usersRepo.findByEmail(email).get(0));
         usersRepo.findByEmail(email).get(0).setGameTable(gameTableRepo.findByIdGameTable(IdGameTable).getIdGameTable());
+        cardRepo.deleteAll(usersRepo.findByEmail(email).get(0).getCard());
+        for (int i = 0; i < 36; i++) {
+            Card card = new Card(
+                    nullPackCardRepo.findByIdCard(i).get(0).getNameCard(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getDataBegSession(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getDataEndSession(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getDevelopment(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getAllDevelopment(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getAnalysis(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getAllAnalysis(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getTesting(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getAllTesting(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getMoney(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getSubs(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getColorCard(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getStatus(),
+                    nullPackCardRepo.findByIdCard(i).get(0).getPriority(),
+                    email);
+            usersRepo.findByEmail(email).get(0).addCard(card);
+            cardRepo.save(card);
+            usersRepo.save(usersRepo.findByEmail(email).get(0));
+        }
         usersRepo.save(usersRepo.findByEmail(email).get(0));
         gameTableRepo.save(gameTableRepo.findByIdGameTable(IdGameTable));
         return gameTableRepo.findByIdGameTable(IdGameTable).getNumberOfPlayers()-gameTableRepo.findByIdGameTable(IdGameTable).getUser().size() ;
@@ -71,10 +95,10 @@ public class GameTableController {
     @JsonView(View.Events.class)
     @PostMapping("/newDay")
     public Events newDay(@RequestParam Integer idGameTable) {
+        gameTableRepo.findByIdGameTable(idGameTable).setStatus(TableStatus.Game);
+        gameTableRepo.findByIdGameTable(idGameTable).setDay(gameTableRepo.findByIdGameTable(idGameTable).getDay() + 1);
+        List<Users> users = List.copyOf(gameTableRepo.findByIdGameTable(idGameTable).getUser());
         if (gameTableRepo.findByIdGameTable(idGameTable).getDay() < 21) {
-            gameTableRepo.findByIdGameTable(idGameTable).setStatus(TableStatus.Game);
-            gameTableRepo.findByIdGameTable(idGameTable).setDay(gameTableRepo.findByIdGameTable(idGameTable).getDay() + 1);
-            List<Users> users = List.copyOf(gameTableRepo.findByIdGameTable(idGameTable).getUser());
             for (int i = 0; i < gameTableRepo.findByIdGameTable(idGameTable).getUser().size(); i++) {
                 usersRepo.findByEmail(users.get(i).getEmail()).get(0).newDay();
                 usersRepo.save(usersRepo.findByEmail(users.get(i).getEmail()).get(0));
@@ -83,10 +107,17 @@ public class GameTableController {
             return eventsRepo.findByDay(gameTableRepo.findByIdGameTable(idGameTable).getDay());
         } else {
             gameTableRepo.findByIdGameTable(idGameTable).setStatus(TableStatus.End);
+            if(gameTableRepo.findByIdGameTable(idGameTable).getDay()>21){
+                gameTableRepo.delete(gameTableRepo.findByIdGameTable(idGameTable));
+            }
+            for (int i = 0; i < gameTableRepo.findByIdGameTable(idGameTable).getUser().size(); i++) {
+                usersRepo.findByEmail(users.get(i).getEmail()).get(0).newDay();
+                usersRepo.save(usersRepo.findByEmail(users.get(i).getEmail()).get(0));
+            }
             gameTableRepo.save(gameTableRepo.findByIdGameTable(idGameTable));
             return eventsRepo.findByDay(gameTableRepo.findByIdGameTable(idGameTable).getDay());
         }
-    }
+        }
 
     @JsonView(View.Events.class)
     @GetMapping("/getEvents")
