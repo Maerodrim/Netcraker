4abdff68c1b10d9cb2f3a17c7e74ffc5a13ea176
Maerodrim <stanislav.sergynin@gmail.com>
2020-12-05 19:33:59 +0400

    обновленя до работоспособности

--- laba/src/main/java/com/example/laba/controller/MatrixController.java
+++ laba/src/main/java/com/example/laba/controller/MatrixController.java
@@ -3,10 +3,9 @@ package com.example.laba.controller;
 import com.example.laba.dto.MatrixDto;
 import com.example.laba.service.MatrixService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("MatrixController")
@@ -16,8 +15,8 @@ public class MatrixController {
     private final MatrixService matrixService;
 
     @PostMapping("addition")
-    public MatrixDto additionMatrix(@RequestBody MatrixDto firstDto,
-                                          @RequestBody MatrixDto secondDto) {
-        return matrixService.addition(firstDto, secondDto);
+    public MatrixDto additionMatrix(@RequestBody MatrixDto[] dto) {
+        System.out.println(Arrays.toString(dto));
+        return matrixService.addition(dto[0], dto[1]);
     }
 }
--- laba/src/main/java/com/example/laba/dto/MatrixDto.java
+++ laba/src/main/java/com/example/laba/dto/MatrixDto.java
@@ -1,6 +1,6 @@
 package com.example.laba.dto;
 
-import com.example.laba.model.Matrix;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,7 +11,35 @@ import lombok.Getter;
 @AllArgsConstructor
 @Builder
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MatrixDto {
-    @JsonProperty("Matrix")
-    public Matrix matrix;
+    public MatrixDto() {
+    }
+
+    public MatrixDto(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.elements = new Double[rows * columns];
+    }
+
+    @JsonProperty("rows")
+    private int rows;
+    @JsonProperty("columns")
+    private int columns;
+    @JsonProperty("elements")
+    private Double[] elements;
+
+    public MatrixDto(int rows, int columns, Double[][] elements) {
+        this.rows = rows;
+        this.columns = columns;
+        this.elements = new Double[rows * columns];
+        for (int i = 0; i < rows; i++) {
+            if (columns >= 0) System.arraycopy(elements[i], 0, this.elements, i * columns, columns);
+        }
+    }
+
+    public void set(Object obj, int i, int j) {
+        this.elements[i*columns + j] = (Double) obj;
+    }
 }
+
